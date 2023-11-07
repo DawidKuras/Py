1c22c274782497839bf1c4a94d3b3c46adef7e2d
DawidKuras <w70683@student.wsiz.edu.pl>
2023-11-07 20:51:42 +0100

    Update Lab0.py

--- Lab0/Lab0.py
+++ Lab0/Lab0.py
@@ -1,3 +1,47 @@
-#zadnie 1
+# #zadnie 3
+# a = int(input("Podaj pierwszy bok prostokąta: "))
+# print(a)
+#
+# b = int(input("Podaj drugi bok prostokata: "))
+# print(b)
+#
+# pole = a * b
+# obwod = 2 * a + 2 * b
+#
+# print("pole prostokata: ", pole)
+# print("obowd prostokata: ", obwod)
+# print("obowd prostokata: "+ str(obwod))
+
+#zadanie 4
+
+# #s = int(input("podaj dlugosc drogi: "))
+# import random
+# s = random.randint(1, 100000)
+# print(s)
+#
+# spalanie = float(input("Podaj srednie spalanie w litrach na 100km: "))
+# print(spalanie)
+#
+# zuzycie = s / spalanie
+# koszt = zuzycie * 6.5
+#
+# print("spalone paliwo", zuzycie)
+# print("koszt przejechanej drogi: ", koszt)
 
 
+#zadanie dodatkowe 1
+
+# print("Bedziemy teraz liczyc rownienie liniowe :) ")
+#
+# a = int(input("Podaj wspolczyniik a: "))
+# b = int(input("podaj wspolczynnik b: "))
+# print("a =",a ,"|", "b =",b)
+#
+# if a == 0:
+#     if b == 0:
+#         print('Równanie tożsamościowe')
+#     else:
+#         print('Równanie sprzeczne')
+# else:
+#     x = -b/a
+#     print('x =', x)
