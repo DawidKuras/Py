36665c0f02e8b64c90d94d0484799d9af65a93fb
DawidKuras <w70683@student.wsiz.edu.pl>
2023-12-09 23:13:20 +0100

    Zadanie z LAB 1

---
+++ Lab2.py
@@ -0,0 +1,36 @@
+# #zad1
+#
+# a = int(input("podaj liczbe a: "))
+# b = int(input("podaj liczbe b: "))
+#
+#
+# if(a>=b):
+#     while b<=a:
+#         if (b % 2 == 1):
+#             b += 1
+#             continue
+#         print(b)
+#         b+=1
+# elif b>=a:
+#     while a <= b:
+#         if (a % 2 == 1):
+#             a += 1
+#             continue
+#         print(a)
+#         a += 1
+
+
+#zad2
+#
+#i=-4.5
+#
+# while i<=4:
+#     i = i + 0.5
+#     if(i!=0 and i<=4):
+#         y = 2*(i*i)-5*i-8
+#         print(y)
+
+# #zad3
+# a = 1
+# while a >= 0:
+#     a = int(input("Podaj liczbe: "))
